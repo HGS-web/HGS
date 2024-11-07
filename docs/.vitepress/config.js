8e12bee951e6c5dd980b07e5f906234f2ec83f50
HGS-web <hgswebgit@gmail.com>
2024-11-07 22:54:19 +0200

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -13,8 +13,57 @@ export default defineConfig({
       description: 'Welcome to the Hellenic Geographical Society website.',
       themeConfig: {
         nav: [
-          {text: 'English', link: '/en/'},
-          {text: 'Ελληνικά', link: '/el/'}
+          {
+            text: 'The Society',
+            items: [
+              { text: 'History', link: 'en/The Society/History' },
+              { text: 'Function', link: 'en/The Society/Function' },
+              { text: 'Goals', link: 'en/The Society/Goals' },
+              { text: 'Submission', link: 'en/The Society/Submission' },
+              { text: 'Donations', link: 'en/The Society/Donations' },
+            ],
+          },
+          {
+            text: 'Collaborations',
+            items: [
+              { text: 'Academic and Scientific Bodies', link: 'en/Collaborations/Academic and Scientific Bodies' },
+              { text: 'Geographical Societies and Networks', link: 'en/Collaborations/Geographical Societies and Networks' },
+              { text: 'Other', link: 'en/Collaborations/Other' },
+            ],
+          },
+          {
+            text: 'Archive',
+            items: [
+              { text: 'Publications', link: 'en/Archive/Publications' },
+              { text: 'Library', link: 'en/Archive/Library' },
+              { text: 'Map Collection', link: 'en/Archive/Map Collection' },
+              { text: 'Photos', link: 'en/Archive/Photos' },
+              { text: 'Reports', link: 'en/Archive/Reports' },
+            ],
+          },
+          {
+            text: 'Conferences',
+            items: [
+              { text: 'GSG', link: 'en/Conferences/HGS' },
+              { text: 'Other', link: 'en/Conferences/Other' },
+            ],
+          },
+          {
+            text: 'Contact us',
+            items: [
+              { text: 'Email', link: 'en/Contact us/Email' },
+              { text: 'Social Media', link: 'en/Contact us/Social Media' },
+              { text: 'Address', link: 'en/Contact us/Address' },
+            ],
+          },
+          {
+            text: 'Extra',
+            items: [
+              { text: 'News', link: 'en/Extra/News' },
+              { text: 'Events', link: 'en/Extra/Events' },
+              { text: 'GSG Conference 2024', link: 'en/Extra/GSG Conference 2024' },
+            ],
+          },
         ]
       }
     },

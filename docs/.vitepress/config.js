fa7a7dd03884503d8300e1cbc0b326c05eab18a8
HGS-web <hgswebgit@gmail.com>
2025-11-06 17:03:45 +0200

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -149,59 +149,6 @@ export default defineConfig({
           message: 'Developed by Alexandros P. Liaskos',
           copyright: 'Copyright © 2024 Hellenic Geographical Society'
         },
-        sidebar: [
-          {
-            text: 'The Society',
-            items: [
-              { text: 'History', link: 'en/The Society/History' },
-              { text: 'Function', link: 'en/The Society/Function' },
-              { text: 'Goals', link: 'en/The Society/Goals' },
-              { text: 'Submission', link: 'en/The Society/Submission' },
-              { text: 'Donations', link: 'en/The Society/Donations' },
-            ],
-          },
-          {
-            text: 'Collaborations',
-            items: [
-              { text: 'Academic and Scientific Bodies', link: 'en/Collaborations/Academic and Scientific Bodies' },
-              { text: 'Geographical Societies and Networks', link: 'en/Collaborations/Geographical Societies and Networks' },
-              { text: 'Other', link: 'en/Collaborations/Other' },
-            ],
-          },
-          {
-            text: 'Archive',
-            items: [
-              { text: 'Publications', link: 'en/Archive/Publications' },
-              { text: 'Library', link: 'en/Archive/Library' },
-              { text: 'Map Collection', link: 'en/Archive/Map Collection' },
-              { text: 'Photos', link: 'en/Archive/Photos' },
-              { text: 'Reports', link: 'en/Archive/Reports' },
-            ],
-          },
-          {
-            text: 'Conferences',
-            items: [
-              { text: 'GSG', link: 'en/Conferences/HGS' },
-              { text: 'Other', link: 'en/Conferences/Other' },
-            ],
-          },
-          {
-            text: 'Contact us',
-            items: [
-              { text: 'Email', link: 'en/Contact us/Email' },
-              { text: 'Social Media', link: 'en/Contact us/Social Media' },
-              { text: 'Address', link: 'en/Contact us/Address' },
-            ],
-          },
-          {
-            text: 'Extra',
-            items: [
-              { text: 'News', link: 'en/Extra/News' },
-              { text: 'Events', link: 'en/Extra/Events' },
-              { text: 'GSG Conference 2024', link: 'en/Extra/GSG Conference 2024' },
-            ],
-          },
-        ],
       },
     },
     el: {
@@ -272,59 +219,6 @@ export default defineConfig({
           message: 'Δημιουργός: Αλέξανδρος Π. Λιάσκος',
           copyright: 'Copyright © 2024 Hellenic Geographical Society'
         },
-        sidebar: [
-          {
-            text: 'Η Εταιρεία',
-            items: [
-              { text: 'Ιστορία', link: '/el/The Society/History' },
-              { text: 'Λειτουργία', link: '/el/The Society/Function' },
-              { text: 'Στόχοι', link: '/el/The Society/Goals' },
-              { text: 'Εγγραφή', link: '/el/The Society/Submission' },
-              { text: 'Δωρεές', link: '/el/The Society/Donations' },
-            ],
-          },
-          {
-            text: 'Συνεργασίες',
-            items: [
-              { text: 'Ακαδημαϊκοί και Επιστημονικοί Φορείς', link: '/el/Collaborations/Academic and Scientific Bodies' },
-              { text: 'Γεωγραφικές Εταιρείες και Δίκτυα', link: '/el/Collaborations/Geographical Societies and Networks' },
-              { text: 'Άλλα', link: '/el/Collaborations/Other' },
-            ],
-          },
-          {
-            text: 'Αρχείο',
-            items: [
-              { text: 'Δημοσιεύσεις', link: '/el/Archive/Publications' },
-              { text: 'Βιβλιοθήκη', link: '/el/Archive/Library' },
-              { text: 'Χαρτοθήκη', link: '/el/Archive/Map Collection' },
-              { text: 'Φωτογραφίες', link: '/el/Archive/Photos' },
-              { text: 'Εκθέσεις', link: '/el/Archive/Reports' },
-            ],
-          },
-          {
-            text: 'Συνέδρια',
-            items: [
-              { text: 'ΕΓΕ', link: '/el/Conferences/HGS' },
-              { text: 'Άλλα', link: '/el/Conferences/Other' },
-            ],
-          },
-          {
-            text: 'Επικοινωνία',
-            items: [
-              { text: 'Ηλεκτρονικό ταχυδρομείο', link: '/el/Contact us/Email' },
-              { text: 'Κοινωνικά Δίκτυα', link: '/el/Contact us/Social Media' },
-              { text: 'Διεύθυνση', link: '/el/Contact us/Address' },
-            ],
-          },
-          {
-            text: 'Extra',
-            items: [
-              { text: 'Νέα', link: '/el/Extra/News' },
-              { text: 'Εκδηλώσεις', link: '/el/Extra/Events' },
-              { text: 'Συνέδριο ΕΓΕ 2024', link: '/el/Extra/GSG Conference 2024' },
-            ],
-          },
-        ],
       },
     },
   },

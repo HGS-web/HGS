9621a5391ca03795f11c4272f5420ffe2bd722e7
Alexandros Liaskos <alexliaskosga@gmail.com>
2026-01-30 17:00:20 +0200

    Add Vercel Analytics to app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${inter.variable} ${jetbrainsMono.variable} antialiased bg-white text-black min-h-screen font-sans`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );

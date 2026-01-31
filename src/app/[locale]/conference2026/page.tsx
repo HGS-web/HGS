01f649308216e5d1e33a3d32f22be9b6797cf5db
Alexandros Liaskos <alexliaskosga@gmail.com>
2026-01-31 13:34:58 +0200

    Add committees to conference 2026 page

--- src/app/[locale]/conference2026/page.tsx
+++ src/app/[locale]/conference2026/page.tsx
@@ -20,8 +20,10 @@ const conferenceText = {
     dateValue: "27-28 November 2026",
     locationLabel: "Location",
     locationValue: "Athens, Greece",
+    committeeLabel: "Organizing Committee",
+    scientificLabel: "Scientific Committee",
     contactLabel: "Contact",
-    contactBlurb: "For more information, please contact us",
+    contactBlurb: "For any question relating the call for sessions, please contact at:",
   },
   el: {
     title: "13th International Conference of the Hellenic Geographical Society",
@@ -30,10 +32,341 @@ const conferenceText = {
     dateValue: "27-28 November 2026",
     locationLabel: "Location",
     locationValue: "Athens, Greece",
+    committeeLabel: "Organizing Committee",
+    scientificLabel: "Scientific Committee",
     contactLabel: "Contact",
-    contactBlurb: "For more information, please contact us",
+    contactBlurb: "For any question relating the call for sessions, please contact at:",
   },
 };
+
+const organizingCommittee = [
+  {
+    designation: "Chair",
+    name: "Em. Prof. T. Maloutas",
+    affiliation: "Harokopio University of Athens",
+    url: "https://www.geo.hua.gr/en/staff-member/maloutas-thomas/",
+  },
+  {
+    designation: "Vice-Chair",
+    name: "Prof. A. Papadopoulos",
+    affiliation: "Harokopio University of Athens",
+    url: "https://www.geo.hua.gr/en/staff-member/papadopoulos-g-apostolos/",
+  },
+  {
+    designation: "Vice-Chair",
+    name: "Dr. G. Saitis",
+    affiliation: "National & Kapodistrian University of Athens",
+    url: "https://gis.geol.uoa.gr/staff/teaching_staff/giannis_saitis",
+  },
+  {
+    designation: "Vice-Chair",
+    name: "Dr. S. Spyrellis",
+    affiliation: "National Centre for Social Research",
+    url: "https://www.ekke.gr/en/centre/personnel/spirellis-stauros-nikiforos",
+  },
+  {
+    designation: "Conference Secretary",
+    name: "Dr. E. Karkani",
+    affiliation: "National & Kapodistrian University of Athens",
+    url: "https://gis.geol.uoa.gr/staff/teaching_staff/anna_karkani",
+  },
+  {
+    designation: "Treasurer",
+    name: "Dr. I. Polyzou",
+    affiliation: "National Technical University of Athens",
+    url: "https://www.arch.ntua.gr/index.php/person/polyzoy-iris/",
+  },
+  {
+    designation: "Special Issue Editor",
+    name: "Prof. C. Koutsopoulos",
+    affiliation: "National Technical University of Athens",
+    url: "https://eurogeojournal.eu/index.php/egj/about/editorialTeam",
+  },
+  {
+    designation: "Field Trip Coordinator",
+    name: "Prof. N. Evelpidou",
+    affiliation: "National & Kapodistrian University of Athens",
+    url: "http://scholar.uoa.gr/evelpidou/home",
+  },
+  {
+    designation: "Webmaster",
+    name: "A. Liaskos",
+    affiliation: "National & Kapodistrian University of Athens",
+  },
+  {
+    designation: "Committee Member",
+    name: "A. Christaki",
+    affiliation: "National Technical University of Athens",
+  },
+  {
+    designation: "Committee Member",
+    name: "Dr. E. Durie",
+    affiliation: "Harokopio University of Athens",
+    url: "https://www.geo.hua.gr/en/staff-member/durie-eveline/",
+  },
+  {
+    designation: "Committee Member",
+    name: "Dr. G. Kandylis",
+    affiliation: "National Centre for Social Research",
+    url: "https://www.ekke.gr/en/centre/personnel/kandilis-georgios",
+  },
+  {
+    designation: "Committee Member",
+    name: "Dr. N. Myofa",
+    affiliation: "Harokopio University of Athens",
+  },
+  {
+    designation: "Committee Member",
+    name: "Y. Paraskevopoulos",
+    affiliation: "National Technical University of Athens",
+  },
+  {
+    designation: "Committee Member",
+    name: "Dr. M. Pigaki (President of HGS)",
+    affiliation: "National Technical University of Athens",
+    url: "https://www.survey.ntua.gr/en/laboratory-staff/pigaki-maria",
+  },
+];
+
+const scientificCommittee = [
+  {
+    name: "Sidiropoulos Giorgios",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=geos",
+  },
+  {
+    name: "Chorianopoulos Ioannis",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=ichorian",
+  },
+  {
+    name: "Petrakou Ilektra",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=ipetr",
+  },
+  {
+    name: "Petropoulou Christy",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=christy.p",
+  },
+  {
+    name: "Stratigea Anastasia",
+    url: "https://www.survey.ntua.gr/en/dep/stratigea-anastasia",
+  },
+  {
+    name: "Koutrolikou Penny",
+    url: "https://www.arch.ntua.gr/index.php/person/koutrolikou-panagiota-penny/?lang=en",
+  },
+  {
+    name: "Micha Irini",
+    url: "https://www.arch.ntua.gr/index.php/person/micha-irini/?lang=en",
+  },
+  {
+    name: "Kandylis Giorgos",
+    url: "https://www.ekke.gr/en/centre/personnel/kandilis-georgios",
+  },
+  {
+    name: "Fereniki Vatavali",
+    url: "https://www.ekke.gr/en/centre/personnel/vatavali-fereniki",
+  },
+  {
+    name: "Baltas Pavlos",
+    url: "https://www.ekke.gr/en/centre/personnel/mpaltas-paulos",
+  },
+  {
+    name: "Pratsinakis Manolis",
+    url: "https://geo.hua.gr/en/personnel/manolis-pratsinakis-2/",
+  },
+  {
+    name: "Tsilimigkas Georgios",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=gtsil",
+  },
+  {
+    name: "Mavrommatis Giorgos",
+    url: "https://geo.hua.gr/en/personnel/giorgos-mavrommatis-2/",
+  },
+  {
+    name: "Faka Antigoni",
+    url: "https://geo.hua.gr/en/personnel/antigoni-faka-2/",
+  },
+  {
+    name: "Panagiotis Artelaris",
+    url: "https://geo.hua.gr/en/personnel/artelaris-panagiotis-2/",
+  },
+  {
+    name: "Drakou Evangelia",
+    url: "https://geo.hua.gr/en/personnel/evangelia-drakou-2/",
+  },
+  {
+    name: "Tragaki Alexandra",
+    url: "https://geo.hua.gr/en/personnel/alexandra-tragaki/",
+  },
+  {
+    name: "Paidakaki Angeliki",
+    url: "https://geo.hua.gr/en/personnel/angeliki-paidakaki-2/",
+  },
+  {
+    name: "Metaxides Nikolas",
+    url: "https://geo.hua.gr/en/personnel/metaxidis-nikos/",
+  },
+  {
+    name: "Lagarias Apostolos",
+    url: "http://www.prd.uth.gr/en/staff/lagarias_a/",
+  },
+  {
+    name: "Vergou Pinelopi",
+    url: "https://www.econ.uth.gr/en/department/staff/special-teaching-staff/147-laboratory-teaching-staff/82-dr-pinelopi-vergou",
+  },
+  {
+    name: "Kourachanis Nikos",
+    url: "https://socialpolicy.panteion.gr/en/people/nkourachanis",
+  },
+  {
+    name: "Hara Kouki",
+    url: "https://sociology.soc.uoc.gr/en/human-resources/teaching-research-staff-dep/kouki-hara-en/",
+  },
+  {
+    name: "Arampatzi Athina",
+  },
+  {
+    name: "Loukas Triantis",
+    url: "https://architecture.web.auth.gr/en/27134-2/",
+  },
+  {
+    name: "Evangelia Athanassiou",
+    url: "https://architecture.web.auth.gr/en/athanasiou-euangelia/",
+  },
+  {
+    name: "Charis Christodoulou",
+    url: "https://architecture.web.auth.gr/en/christodoulou-charis/",
+  },
+  {
+    name: "Konstantina Kalfa",
+    url: "https://www.asfa.gr/didaktiko-prosopiko/konstantina-kalfa/",
+  },
+  {
+    name: "Chadjimichalis Costis",
+    url: "https://geo.hua.gr/en/personnel/chadjimichalis-costis/",
+  },
+  {
+    name: "Kostas Gourzis",
+    url: "https://lgrl.aegean.gr/el/personnel/gourzis-kostas/",
+  },
+  {
+    name: "Iosifidis Theodoros",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=thiwsi",
+  },
+  {
+    name: "Gavalas Vassilis",
+    url: "https://geography.aegean.gr/ppl/index.php?content=0&bio=bgav",
+  },
+  {
+    name: "Efthimios Bakogiannis",
+    url: "https://www.survey.ntua.gr/en/dep/bakogiannis-euthimios",
+  },
+  {
+    name: "Maria Papadopoulou",
+    url: "https://www.survey.ntua.gr/en/dep/papadopoulou-maria",
+  },
+  {
+    name: "Aspassia Kouzoupi",
+    url: "https://www.arch.uth.gr/el/staff/A_Kouzoupi",
+  },
+  {
+    name: "Dimitrios Alexakis",
+    url: "https://www.ims.forth.gr/en/profile/view?id=97",
+  },
+  {
+    name: "Andreas Tsatsaris",
+    url: "https://geo.uniwa.gr/en/profile/tsatsaris-andreas/",
+  },
+  {
+    name: "Athena Athanasiou",
+    url: "https://anthropology.panteion.gr/index.php/el/dep/198-athanasiou-athena",
+  },
+  {
+    name: "Soulakellis Nikolaos",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=nsoul",
+  },
+  {
+    name: "Gialis Stelios",
+    url: "https://geography.aegean.gr/ppl/index_en.php?content=0&bio=stgialis",
+  },
+  {
+    name: "Nicos Souliotis",
+    url: "https://www.ekke.gr/en/centre/personnel/souliotis-nikolaos",
+  },
+  {
+    name: "Giorgos Bithymitris",
+    url: "https://www.ekke.gr/en/centre/personnel/mpithimitris-georgios",
+  },
+  {
+    name: "Christos Chalkias",
+  },
+  {
+    name: "Konstantinos Tsanakas",
+  },
+  {
+    name: "George Petropoulos",
+    url: "https://geo.hua.gr/en/personnel/petropoulos-george-p/",
+  },
+  {
+    name: "Ifigeneia Kokkali",
+    url: "https://www.prd.uth.gr/en/staff/kokkali_i/",
+  },
+  {
+    name: "Vassilis Arapoglou",
+    url: "https://socialpolicy.panteion.gr/en/people/varapoglou",
+  },
+  {
+    name: "Akrivi Leka",
+    url: "https://www.survey.ntua.gr/en/laboratory-staff/leka-akrivi",
+  },
+  {
+    name: "Michalis Vaitis",
+    url: "https://geography.aegean.gr/ppl/index.php?content=0&bio=vaitis",
+  },
+  {
+    name: "Nikolaos Zouros",
+    url: "https://geography.aegean.gr/ppl/index.php?content=0&bio=nzour",
+  },
+  {
+    name: "Athanasios Kizos",
+    url: "https://geography.aegean.gr/ppl/index.php?content=0&bio=akizos",
+  },
+  {
+    name: "Tscheulin Thomas",
+    url: "https://geography.aegean.gr/ppl/index.php?content=0&bio=t.tscheulin",
+  },
+  {
+    name: "Dimitris Kavroudakis",
+    url: "https://geography.aegean.gr/ppl/index.php?content=0&bio=dimitrisk",
+  },
+  {
+    name: "Efi Kostopoulou",
+    url: "https://geography.aegean.gr/ppl/index.php?content=0&bio=ekost",
+  },
+  {
+    name: "Lia Galani",
+    url: "http://www.primedu.uoa.gr/galanh-apostolia-lia.html",
+  },
+  {
+    name: "Charalambos Fasoulas",
+    url: "https://www.nhmc.uoc.gr/archives/nhmc-person/charalampos-fassoulas#:~:text=%CE%A3%CF%8D%CE%BD%CF%84%CE%BF%CE%BC%CE%BF%20%CE%92%CE%B9%CE%BF%CE%B3%CF%81%CE%B1%CF%86%CE%B9%CE%BA%CF%8C,%CE%A6.",
+  },
+  {
+    name: "Nikos Labrinos",
+    url: "https://eled.auth.gr/about/personnel/lambrinos/",
+  },
+  {
+    name: "Angeliki Rokka",
+    url: "https://eled.duth.gr/author/arokka/",
+  },
+  {
+    name: "Katerina Gkoltsiou",
+    url: "http://efp.aua.gr/el/userpage/2395",
+  },
+  {
+    name: "Pavlis Evangelos",
+    url: "https://aoa.aua.gr/?teacher=pavlis-evangelos",
+  },
+];
 
 export default async function Conference2026Page({ params }: PageProps) {
   const { locale } = await params;
@@ -103,17 +436,88 @@ export default async function Conference2026Page({ params }: PageProps) {
             </FadeInView>
           )}
 
+          {/* Organizing Committee */}
+          <FadeInView delay={0.15}>
+            <div className="mt-8 rounded-2xl border border-black/10 bg-white p-6 shadow-sm">
+              <h3 className="text-xl font-semibold text-black mb-6">{t.committeeLabel}</h3>
+              <div className="overflow-x-auto">
+                <table className="min-w-full text-left text-sm text-black/70">
+                  <thead className="text-xs uppercase tracking-wide text-black/50">
+                    <tr>
+                      <th className="py-2 pr-4">Designation</th>
+                      <th className="py-2 pr-4">Name</th>
+                      <th className="py-2">Affiliation</th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-black/10">
+                    {organizingCommittee.map((member) => (
+                      <tr key={`${member.designation}-${member.name}`}>
+                        <td className="py-3 pr-4 font-medium text-black">
+                          {member.designation}
+                        </td>
+                        <td className="py-3 pr-4">
+                          {member.url ? (
+                            <a
+                              href={member.url}
+                              target="_blank"
+                              rel="noreferrer"
+                              className="text-black hover:underline"
+                            >
+                              {member.name}
+                            </a>
+                          ) : (
+                            member.name
+                          )}
+                        </td>
+                        <td className="py-3 text-black/70">{member.affiliation}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </FadeInView>
+
+          {/* Scientific Committee */}
+          <FadeInView delay={0.18}>
+            <div className="mt-8 rounded-2xl border border-black/10 bg-white p-6 shadow-sm">
+              <h3 className="text-xl font-semibold text-black mb-6">{t.scientificLabel}</h3>
+              <div className="flex flex-wrap gap-3">
+                {scientificCommittee.map((member) =>
+                  member.url ? (
+                    <a
+                      key={member.name}
+                      href={member.url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="rounded-full border border-black/10 bg-black/5 px-4 py-2 text-sm text-black/70 hover:border-black/20 hover:bg-black/10"
+                    >
+                      {member.name}
+                    </a>
+                  ) : (
+                    <span
+                      key={member.name}
+                      className="rounded-full border border-black/10 bg-black/5 px-4 py-2 text-sm text-black/70"
+                    >
+                      {member.name}
+                    </span>
+                  )
+                )}
+              </div>
+            </div>
+          </FadeInView>
+
           {/* Contact */}
           <FadeInView delay={0.2}>
             <div className="mt-8 p-8 rounded-2xl border border-black/10 bg-white shadow-sm text-center">
               <h3 className="text-xl font-semibold text-black mb-4">{t.contactLabel}</h3>
               <p className="text-black/60 mb-6">{t.contactBlurb}</p>
               <a
-                href="mailto:info@geographiki.gr"
+                href="mailto:ekarkani@geol.uoa.gr"
                 className="inline-flex items-center gap-2 px-6 py-3 bg-black text-white font-medium rounded-full hover:bg-black/90 transition-all"
               >
                 <Mail className="h-4 w-4" />
-                info@geographiki.gr
+                ekarkani@geol.uoa.gr
               </a>
             </div>
           </FadeInView>
